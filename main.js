8d93466cded6f27cf893f7f7a087578efc5e459f
= <=>
2022-08-07 21:22:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function parse() {
+    const keywords = ["IS", "USES", "HAS", "BASE", "ARE", "PLUS", "AND", "BUT"];
+    const textEnclosers = ["'", '"'];
+    const colourInit = "#";
+    // ON ADD:
+    // document.getElementsByTagName('h2')[3].scrollIntoView();
+}
